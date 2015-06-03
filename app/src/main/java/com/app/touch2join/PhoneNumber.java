7f37e661e26b74f8ce8f07bfcf40613e3d3cd749
valerysigalov <valery.sigalov@gmail.com>
2015-06-02 22:58:06 -0400

    1. Fix regex to support xxx-xxx-xx-xx format.

--- app/src/main/java/com/app/touch2join/PhoneNumber.java
+++ app/src/main/java/com/app/touch2join/PhoneNumber.java
@@ -27,7 +27,7 @@ class PhoneNumber {
     private static final String us_country_code = ".*?(((\\+\\s*1\\s*(?:\\-|\\.?)\\s*";
     private static final String area_code = "\\(?\\s*[0-9]{3}\\s*\\)?\\s*(?:\\-|\\.?)\\s*";
     private static final String us_toll_free_codes = "\\(?\\s*(800|888|877|866|855|844)\\s*\\)?\\s*(?:\\-|\\.?)\\s*";
-    private static final String phone_number = "\\(?\\s*[0-9]{3}\\s*\\)?\\s*(?:\\-|\\.?)\\s*\\(?\\s*[0-9]{4}\\s*\\)?))).*";
+    private static final String phone_number = "\\(?\\s*[0-9]{3}\\s*\\)?\\s*(?:\\-|\\.?)\\s*\\(?\\s*[0-9]{2}\\s*\\)?\\s*(?:\\-|\\.?)\\s*\\(?\\s*[0-9]{2}\\s*\\)?))).*";
     private static final String pin_code = ".*?((\\D|\\s)([0-9]{5,8})(\\D|\\s|$)).*";
     private static final String pin_code_ex = ".*(Access|Pin|Code|Id|Conference)(\\W*?)([[0-9]\\s\\.\\)\\(-]+)(#|\\n|$).*";
     private static final String us_pattern = "(\\bUSA\\b|\\bUNITED STATES\\b|\\bUnited States\\b)";
